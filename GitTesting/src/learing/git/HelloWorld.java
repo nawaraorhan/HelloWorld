482411a37cc2bcd004a0124836ad0742dffc991d
nawaraorhan <nawara.orhan@gmail.com>
2019-05-23 21:35:06 -0400

    first time commit

---
+++ GitTesting/src/learing/git/HelloWorld.java
@@ -0,0 +1,12 @@
+package learing.git;
+
+import org.omg.Messaging.SyncScopeHelper;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		 
+       System.out.println("HELLO WORLD");
+	}
+
+}
